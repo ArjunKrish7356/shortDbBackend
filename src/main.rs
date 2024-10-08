82d1285656551444fd1228c8dfb13cd7c7cbfe35
ArjunKrish7356 <arjunkrishna7356@gmail.com>
2024-10-09 00:12:42 +0530

    backend phase1 completed

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
+use actix_cors::Cors;
+use actix_web::http;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use tonic::transport::Channel;
-use std::sync::Arc;
+
 use proto::basic_client::BasicClient;
 use serde::Deserialize;
+use std::sync::Arc;
+use tonic::transport::Channel;
 pub mod methods;
 
 pub mod proto {
     tonic::include_proto!("commands"); // The string here must match the package name in your .proto file
 }
-
 
 #[derive(Deserialize)]
 struct Get {
@@ -21,16 +23,22 @@ struct Set {
     value: String,
 }
 
+#[get("/{key}")]
+async fn getDataFromDb(path: web::Path<String>) -> impl Responder {
+    let key = path.into_inner(); // Get the key from the path
 
-#[get("/get")]
-async fn getDataFromDb(
-    web::Query(data): web::Query<Get>, 
-) -> impl Responder {
-    println!("Received GET request for key: {}", data.key);
-    let response = methods::get::get(data.key.to_string()).await;
+    println!("Received GET request for key: {}", key);
+
+    // Retrieve the corresponding URL for the key
+    let response = methods::get::get(key).await;
 
     match response {
-        Ok(result) => HttpResponse::Ok().body(result),
+        Ok(url) => {
+            println!("Redirecting to: {}", url);
+            HttpResponse::Found() // 302 Found for redirection
+                .header("Location", url) // Redirect to the URL
+                .finish()
+        }
         Err(e) => {
             eprintln!("Error: {}", e);
             HttpResponse::InternalServerError().body(format!("Error: {}", e))
@@ -39,9 +47,7 @@ async fn getDataFromDb(
 }
 
 #[post("/set")]
-async fn setDataToDb(
-    data: web::Json<Set>, 
-) -> impl Responder {
+async fn setDataToDb(data: web::Json<Set>) -> impl Responder {
     let key = &data.key;
     let value = &data.value;
 
@@ -55,11 +61,16 @@ async fn setDataToDb(
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let url = "http://[::1]:50051";
-    let client = BasicClient::connect(url).await.unwrap();
-
     HttpServer::new(move || {
         App::new()
+            .wrap(
+                Cors::default()
+                    .allowed_origin("http://localhost:5173") // Update with your frontend URL
+                    .allowed_methods(vec!["GET", "POST", "OPTIONS"])
+                    .allowed_headers(vec![http::header::CONTENT_TYPE, http::header::ACCEPT])
+                    .supports_credentials()
+                    .max_age(3600),
+            )
             .service(setDataToDb)
             .service(getDataFromDb)
     })
